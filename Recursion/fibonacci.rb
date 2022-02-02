fe2a50653e61cf5e63bb441e9553aadbe04e8d69
samira-sadat48 <samira.sadat48@gmail.com>
2022-02-02 12:30:38 -0700

    Fixed loop counter on iterative func and created recursive func

--- Recursion/fibonacci.rb
+++ Recursion/fibonacci.rb
@@ -2,11 +2,23 @@ def fibs(number)
     a = 0
     b = 1
     fib = []
-    while a < number
+    (1..number).each do
         fib.push(a)
         a,b = b, a+b
     end
     return fib
 end
 
-puts fibs(10)
+def fibs_rec(number)
+    return [] if number == 0
+    return [0] if number == 1
+    return [0,1] if number == 2
+
+    fib = fibs_rec(number - 1)
+    fib << fib[-2] + fib[-1]
+
+    return fib
+end
+
+#puts fibs(10)
+puts fibs_rec(10)
