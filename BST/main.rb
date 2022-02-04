0af2781cfd26a66480420c465f609565f251041f
samira-sadat48 <samira.sadat48@gmail.com>
2022-02-04 14:35:36 -0700

    Created delete function and all helper functions for each child format

--- BST/main.rb
+++ BST/main.rb
@@ -4,6 +4,8 @@ data = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 tree = Tree.new(data)
 
 #puts tree.data
-#tree.pretty_print
-tree.insert(100)
+tree.pretty_print
+tree.insert(10)
+tree.pretty_print
+tree.delete(4)
 tree.pretty_print
--- BST/tree.rb
+++ BST/tree.rb
@@ -14,13 +14,36 @@ class Tree
         pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
     end
 
-    def insert(value, node = root)
+    def insert(value, node = @root)
         return nil if value == node.data
 
         if value < node.data
             node.left.nil? ? node.left = Node.new(value) : insert(value,node.left)
         else
             node.right.nil? ? node.right = Node.new(value) : insert(value,node.right)
+        end
+    end
+
+    def find(value, current = @root)
+        return current if current.data == value
+
+        current.data > value ? find(value, current.left) : find(value, current.right)
+    end
+
+    def delete(value, node = @root)
+        return node if node.nil?
+        
+        parent_node = parent(value)
+        left_node = find(value).left
+        right_node = find(value).right
+
+        #if the node to be deleted has no children
+        if left_node.nil? && right_node.nil?
+            delete_end_node(parent_node,value)
+        elsif left_node.nil? || right_node.nil?
+            delete_single_child_node(parent_node,value)
+        else
+            delete_double_child_node(parent_node,value)
         end
     end
 
@@ -38,4 +61,40 @@ class Tree
 
         root_node
     end
+
+    def parent(value, current = @root)
+        return nil if @root == value
+        return current if current.left.data == value || current.right.data == value
+
+        current.data > value ? parent(value, current.left) : parent(value, current.right)
+    end
+
+    def delete_end_node(parent,value)
+        parent.data > value ? parent.left = nil : parent.right = nil
+    end
+
+    def delete_single_child_node(parent,value)
+        grandchild = find(value).right if find(value).left.nil?
+        grandchild = find(value).left if find(value).right.nil?
+
+        parent.right = grandchild if parent.right == find(value)
+        parent.left = grandchild if parent.left == find(value)
+    end
+
+    def delete_double_child_node(parent,value)
+        replace = find_inorder_successor(value)
+        delete(replace.data)
+        replace.right = find(value).right
+        replace.left = find(value).left
+        parent.right = replace if parent.right == find(value)
+        parent.left = replace  if parent.left == find(value)
+    end
+
+    def find_inorder_successor(value, successor = value)
+        result = find(successor + 1)
+        return result unless result.nil?
+    
+        find_inorder_successor(successor + 1)
+    end
+
 end
