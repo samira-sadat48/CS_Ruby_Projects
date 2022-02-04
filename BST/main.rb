186e5c819b55b9ea713d733ffec653b4eda9f099
samira-sadat48 <samira.sadat48@gmail.com>
2022-02-04 15:05:02 -0700

    Test Script

--- BST/main.rb
+++ BST/main.rb
@@ -1,11 +1,49 @@
 require_relative 'tree'
 
-data = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-tree = Tree.new(data)
+array = Array.new(15) { rand(1..100) }
+bst = Tree.new(array)
 
-#puts tree.data
-tree.pretty_print
-tree.insert(10)
-tree.pretty_print
-tree.delete(4)
-tree.pretty_print
+bst.pretty_print
+
+puts bst.balanced? ? 'Your Binary Search Tree is balanced.' : 'Your Binary Search Tree is not balanced.'
+
+puts 'Level order traversal: '
+puts bst.level_order
+
+puts 'Preorder traversal: '
+puts bst.preorder
+
+puts 'Inorder traversal: '
+puts bst.inorder
+
+puts 'Postorder traversal: '
+puts bst.postorder
+
+10.times do
+  a = rand(100..150)
+  bst.insert(a)
+  puts "Inserted #{a} to tree."
+end
+
+bst.pretty_print
+
+puts bst.balanced? ? 'Your Binary Search Tree is balanced.' : 'Your Binary Search Tree is not balanced.'
+
+puts 'Rebalancig tree...'
+bst.rebalance
+
+bst.pretty_print
+
+puts bst.balanced? ? 'Your Binary Search Tree is balanced.' : 'Your Binary Search Tree is not balanced.'
+
+puts 'Level order traversal: '
+puts bst.level_order
+
+puts 'Preorder traversal: '
+puts bst.preorder
+
+puts 'Inorder traversal: '
+puts bst.inorder
+
+puts 'Postorder traversal: '
+puts bst.postorder
